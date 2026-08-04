7e6ff0ba3f4b0aff2c6ea43637af1cd0a1fe4216
Man <varmoraman2006@gmail.com>
2026-08-04 21:36:25 +0530

    Add signup and login routes with JWT auth

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -38,4 +38,41 @@ router.post("/signup", async (req, res) => {
     }
 });
 
+
+router.post('/login', async (req,res)=>{
+    try{
+        const {email, password} = req.body;
+
+        if(!email || !password){
+            return res.status(400).json({success: false, message: "Email and Password are Required"});
+        }
+         
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(400).json({success: false, message: "Invalid Email or Password"});
+        }
+
+        const isMatch = await bycrt.compare(password, user.passwordHash);
+        if(!isMatch){
+            return res.status(400).json({success: false, message: "Invalid Email or Password"});
+        }
+
+        const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {
+            expiresIn: "7d",
+        })
+
+        res.status(200).json({
+            success: true,
+            user: { _id: user._id, name: user.name, email: user.email },
+            token,
+        });
+    }
+
+    catch(error){
+        res.status(500).json({success: false, message: error.message});
+    }
+});
+
+
 export default router;
